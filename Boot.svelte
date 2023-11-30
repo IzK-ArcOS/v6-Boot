9e16fc564e0b1b9f32ab20c656de86ed4d0fee90
Izaak Kuipers <izaak.kuipers@gmail.com>
2023-11-30 19:46:51 +0100

    Actually begin working on the bootscreen

--- Boot.svelte
+++ Boot.svelte
@@ -1,3 +1,11 @@
+<script lang="ts">
+  import { Logo } from "$ts/branding";
+  import Bar from "../../lib/Components/Progress/Bar.svelte";
+</script>
+
 <div class="state-boot fullscreen center-flex">
-  <div class="content"></div>
+  <div class="content">
+    <img src={Logo()} alt="" class="logo" />
+    <Bar indeterminate />
+  </div>
 </div>
